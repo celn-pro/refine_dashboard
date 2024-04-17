b69cc24d193fb48f428a468170de689004911a35
celn-pre <ayouberasto7@gmail.com>
2024-04-17 18:13:47 +0300

    initial commit

---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,27 @@
+import { getNameInitials } from "@/utilities"
+import { Avatar as AntAvatar, AvatarProps } from "antd"
+
+type Props = AvatarProps & {
+    name?: string
+}
+
+const CustomAvatar = ({name, style, ...rest}: Props) => {
+  return (
+        <AntAvatar
+            alt={getNameInitials(name || '')}
+            size="small"
+            style={{
+                backgroundColor: '#87d068',
+                display:'flex',
+                alignItems: 'center',
+                border: 'none',
+                ...style
+            }}
+            {...rest}
+        >
+            {name}
+        </AntAvatar>
+  )
+}
+
+export default CustomAvatar
